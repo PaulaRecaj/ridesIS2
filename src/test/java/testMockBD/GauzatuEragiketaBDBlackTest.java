cf9f27a463b586787444f190e619cc104cbeaabb
paularecaj <paularecajgarcia@gmail.com>
2024-10-10 16:44:46 +0200

    Revertidos los cambios de la prueba coverage

--- src/test/java/testMockBD/GauzatuEragiketaBDBlackTest.java
+++ src/test/java/testMockBD/GauzatuEragiketaBDBlackTest.java
@@ -8,144 +8,146 @@ import data_access.DataAccess;
 import testOperations.TestDataAccess;
 
 public class GauzatuEragiketaBDBlackTest {
-	/*
-	 * //sut:system under test static DataAccess sut=new DataAccess();
-	 * 
-	 * //additional operations needed to execute the test static TestDataAccess
-	 * testDA=new TestDataAccess();
-	 * 
-	 * private String username; private boolean deposit; private double amount;
-	 * boolean op;
-	 * 
-	 * 
-	 * @Test //username==null, the test should return false public void test1() {
-	 * 
-	 * username=null; amount=100.00; deposit=true;
-	 * 
-	 * try {
-	 * 
-	 * sut.open();
-	 * 
-	 * op= sut.gauzatuEragiketa(username, amount, deposit);
-	 * 
-	 * assertFalse(op);
-	 * 
-	 * 
-	 * }catch(Exception e) {
-	 * 
-	 * fail();
-	 * 
-	 * }finally {
-	 * 
-	 * sut.close();
-	 * 
-	 * }
-	 * 
-	 * 
-	 * }
-	 * 
-	 * 
-	 *//**
-		 * amount=0 and amount<0 (debería de devolver false, pero como el método
-		 * gauzatuEragiketa no comprueba si amount es igual a 0 o menor que 0, devuelve
-		 * true y por eso falla )
-		 *//*
-			 * @Test public void test2() {
-			 * 
-			 * username="Urtzi"; amount=0.00; double amount2 = -10.00; deposit= true;
-			 * 
-			 * 
-			 * try {
-			 * 
-			 * sut.open();
-			 * 
-			 * op = sut.gauzatuEragiketa(username, amount, deposit);
-			 * 
-			 * 
-			 * assertFalse(op);
-			 * 
-			 * 
-			 * op = sut.gauzatuEragiketa(username, amount2, deposit);
-			 * 
-			 * assertFalse(op);
-			 * 
-			 * 
-			 * 
-			 * }catch(Exception e){
-			 * 
-			 * fail();
-			 * 
-			 * }finally{
-			 * 
-			 * sut.close();
-			 * 
-			 * }
-			 * 
-			 * }
-			 * 
-			 * 
-			 * 
-			 * 
-			 * //the user does not exist in the database
-			 * 
-			 * @Test public void test3() {
-			 * 
-			 * username="Marta"; amount=10.00; deposit=false;
-			 * 
-			 * sut.open();
-			 * 
-			 * try {
-			 * 
-			 * op= sut.gauzatuEragiketa(username, amount, deposit);
-			 * 
-			 * assertFalse(op);
-			 * 
-			 * }catch(Exception e) {
-			 * 
-			 * fail(); }finally {
-			 * 
-			 * sut.close();
-			 * 
-			 * }
-			 * 
-			 * 
-			 * }
-			 * 
-			 * 
-			 * 
-			 * //the user exists in the database, the method should go perfectly and return
-			 * true
-			 * 
-			 * @Test public void test4() {
-			 * 
-			 * username="Zuri"; amount= 10.00; deposit=true;
-			 * 
-			 * try {
-			 * 
-			 * sut.open();
-			 * 
-			 * op = sut.gauzatuEragiketa(username, amount, deposit);
-			 * 
-			 * assertTrue(op);
-			 * 
-			 * 
-			 * deposit= false;
-			 * 
-			 * op = sut.gauzatuEragiketa(username, amount, deposit);
-			 * 
-			 * assertTrue(op);
-			 * 
-			 * 
-			 * 
-			 * }catch(Exception e) {
-			 * 
-			 * fail(); }finally {
-			 * 
-			 * sut.close();
-			 * 
-			 * }
-			 * 
-			 * }
-			 */
+
+	// sut:system under test
+	static DataAccess sut = new DataAccess();
+
+	// additional operations needed to execute the test
+	static TestDataAccess testDA = new TestDataAccess();
+
+	private String username;
+	private boolean deposit;
+	private double amount;
+	boolean op;
+
+	@Test
+	// username==null, the test should return false
+	public void test1() {
+
+		username = null;
+		amount = 100.00;
+		deposit = true;
+
+		try {
+
+			sut.open();
+
+			op = sut.gauzatuEragiketa(username, amount, deposit);
+
+			assertFalse(op);
+
+		} catch (Exception e) {
+
+			fail();
+
+		} finally {
+
+			sut.close();
+
+		}
+
+	}
+
+	/**
+	 * amount=0 and amount<0 (debería de devolver false, pero como el método
+	 * gauzatuEragiketa no comprueba si amount es igual a 0 o menor que 0, devuelve
+	 * true y por eso falla )
+	 */
+	@Test
+	public void test2() {
+
+		username = "Urtzi";
+		amount = 0.00;
+		double amount2 = -10.00;
+		deposit = true;
+
+		try {
+
+			sut.open();
+
+			op = sut.gauzatuEragiketa(username, amount, deposit);
+
+			assertFalse(op);
+
+			op = sut.gauzatuEragiketa(username, amount2, deposit);
+
+			assertFalse(op);
+
+		} catch (Exception e) {
+
+			fail();
+
+		} finally {
+
+			sut.close();
+
+		}
+
+	}
+
+	// the user does not exist in the database
+	@Test
+	public void test3() {
+
+		username = "Marta";
+		amount = 10.00;
+		deposit = false;
+
+		sut.open();
+
+		try {
+
+			op = sut.gauzatuEragiketa(username, amount, deposit);
+
+			assertFalse(op);
+
+		} catch (Exception e) {
+
+			fail();
+		} finally {
+
+			sut.close();
+
+		}
+
+	}
+
+	// the user exists in the database, the method should go perfectly and return
+	// true
+	@Test
+	 public void test4() {
+		 
+		 username="Zuri";
+		 amount= 10.00;
+		 deposit=true;
+		 
+		 try {
+			 
+			 sut.open();
+			 
+			 op = sut.gauzatuEragiketa(username, amount, deposit);
+			 
+			 assertTrue(op);
+			 
+			 
+			 deposit= false;
+			 
+			 op = sut.gauzatuEragiketa(username, amount, deposit);
+			 
+			 assertTrue(op);
+			 
+			 
+			 
+		 }catch(Exception e) {
+			 
+			 fail();
+		 }finally {
+			 
+			 sut.close();
+			 
+		 }
+	}
+	 
+	
 
 }
--- src/test/java/testMockBD/GauzatuEragiketaBDWhiteTest.java
+++ src/test/java/testMockBD/GauzatuEragiketaBDWhiteTest.java
@@ -8,168 +8,181 @@ import data_access.DataAccess;
 import testOperations.TestDataAccess;
 
 public class GauzatuEragiketaBDWhiteTest {
-	
-	/*
-	 * //sut:system under test static DataAccess sut=new DataAccess();
-	 * 
-	 * //additional operations needed to execute the test static TestDataAccess
-	 * testDA=new TestDataAccess();
-	 * 
-	 * 
-	 * private String username; private boolean deposit; private double amount;
-	 * boolean op;
-	 * 
-	 * 
-	 * @Test //user is null. The test should return false public void test1() {
-	 * 
-	 * username=null; amount=25.00; deposit=false;
-	 * 
-	 * try {
-	 * 
-	 * sut.open();
-	 * 
-	 * op= sut.gauzatuEragiketa(username, amount, deposit);
-	 * 
-	 * assertFalse(op);
-	 * 
-	 * 
-	 * }catch(Exception e) {
-	 * 
-	 * fail();
-	 * 
-	 * }finally {
-	 * 
-	 * sut.close();
-	 * 
-	 * } }
-	 * 
-	 * 
-	 * 
-	 * @Test //the user does not exist in the database public void test2() {
-	 * 
-	 * username="Marta"; amount=80.00; deposit=true;
-	 * 
-	 * sut.open();
-	 * 
-	 * try {
-	 * 
-	 * op= sut.gauzatuEragiketa(username, amount, deposit);
-	 * 
-	 * assertFalse(op);
-	 * 
-	 * }catch(Exception e) {
-	 * 
-	 * fail(); }finally {
-	 * 
-	 * sut.close();
-	 * 
-	 * }
-	 * 
-	 * 
-	 * }
-	 * 
-	 * 
-	 * 
-	 * @Test //the user does not withdraw all the money from his account leaving its
-	 * balance >0 public void test3() {
-	 * 
-	 * username="Unax"; amount=5.00; deposit=false;
-	 * 
-	 * try {
-	 * 
-	 * sut.open();
-	 * 
-	 * 
-	 * double dineroAntes= sut.getActualMoney(username);
-	 * 
-	 * System.out.println("dinero antes del retiro: "+dineroAntes);
-	 * 
-	 * op = sut.gauzatuEragiketa(username, amount, deposit);
-	 * 
-	 * double dineroDespues= sut.getActualMoney(username);
-	 * 
-	 * System.out.println("dinero despues del retiro: "+dineroDespues);
-	 * 
-	 * assertTrue(dineroDespues>0);
-	 * 
-	 * 
-	 * }catch(Exception e) {
-	 * 
-	 * fail(); }finally {
-	 * 
-	 * deposit=true; sut.gauzatuEragiketa(username, amount, deposit); sut.close();
-	 * 
-	 * }
-	 * 
-	 * }
-	 * 
-	 * 
-	 * @Test //the user does withdraws all the money from his account leaving its
-	 * balance 0 public void test4() {
-	 * 
-	 * username="Unax"; amount=100.00; deposit=false;
-	 * 
-	 * try {
-	 * 
-	 * sut.open();
-	 * 
-	 * double dineroAntes= sut.getActualMoney(username);
-	 * 
-	 * System.out.println("dinero antes del retiro: "+dineroAntes);
-	 * 
-	 * op = sut.gauzatuEragiketa(username, amount, deposit);
-	 * 
-	 * 
-	 * double dineroDespues= sut.getActualMoney(username);
-	 * 
-	 * System.out.println("dinero despues del retiro: "+dineroDespues);
-	 * 
-	 * assertTrue(dineroDespues==0);
-	 * 
-	 * }catch(Exception e) {
-	 * 
-	 * fail(); }finally {
-	 * 
-	 * deposit=true; sut.gauzatuEragiketa(username, amount, deposit); sut.close();
-	 * 
-	 * }
-	 * 
-	 * }
-	 * 
-	 * 
-	 * @Test //the user deposits money in his account successfully public void
-	 * test5() {
-	 * 
-	 * username="Urtzi"; amount=25.00; deposit=true;
-	 * 
-	 * try {
-	 * 
-	 * sut.open();
-	 * 
-	 * double dineroAntes= sut.getActualMoney(username);
-	 * 
-	 * System.out.println("dinero antes del ingreso: "+dineroAntes);
-	 * 
-	 * op = sut.gauzatuEragiketa(username, amount, deposit);
-	 * 
-	 * double dineroDespues= sut.getActualMoney(username);
-	 * 
-	 * System.out.println("dinero despues del ingreso: "+dineroDespues);
-	 * 
-	 * assertTrue(dineroDespues== dineroAntes+25.00);
-	 * 
-	 * 
-	 * 
-	 * }catch(Exception e) {
-	 * 
-	 * fail(); }finally {
-	 * 
-	 * deposit=false; sut.gauzatuEragiketa(username, amount, deposit); sut.close();
-	 * 
-	 * }
-	 * 
-	 * }
-	 * 
-	 * 
-	 */
+
+	// sut:system under test
+	static DataAccess sut = new DataAccess();
+
+//additional operations needed to execute the test 
+	static TestDataAccess testDA = new TestDataAccess();
+
+	private String username;
+	private boolean deposit;
+	private double amount;
+	boolean op;
+
+	@Test
+//user is null. The test should return false
+	public void test1() {
+
+		username = null;
+		amount = 25.00;
+		deposit = false;
+
+		try {
+
+			sut.open();
+
+			op = sut.gauzatuEragiketa(username, amount, deposit);
+
+			assertFalse(op);
+
+		} catch (Exception e) {
+
+			fail();
+
+		} finally {
+
+			sut.close();
+
+		}
+	}
+
+	@Test
+	// the user does not exist in the database
+	public void test2() {
+
+		username = "Marta";
+		amount = 80.00;
+		deposit = true;
+
+		sut.open();
+
+		try {
+
+			op = sut.gauzatuEragiketa(username, amount, deposit);
+
+			assertFalse(op);
+
+		} catch (Exception e) {
+
+			fail();
+		} finally {
+
+			sut.close();
+
+		}
+
+	}
+
+	@Test
+	// the user does not withdraw all the money from his account leaving its balance
+	// >0
+	public void test3() {
+
+		username = "Unax";
+		amount = 5.00;
+		deposit = false;
+
+		try {
+
+			sut.open();
+
+			double dineroAntes = sut.getActualMoney(username);
+
+			System.out.println("dinero antes del retiro: " + dineroAntes);
+
+			op = sut.gauzatuEragiketa(username, amount, deposit);
+
+			double dineroDespues = sut.getActualMoney(username);
+
+			System.out.println("dinero despues del retiro: " + dineroDespues);
+
+			assertTrue(dineroDespues > 0);
+
+		} catch (Exception e) {
+
+			fail();
+		} finally {
+
+			deposit = true;
+			sut.gauzatuEragiketa(username, amount, deposit);
+			sut.close();
+
+		}
+
+	}
+
+	@Test
+	// the user does withdraws all the money from his account leaving its balance 0
+	public void test4() {
+
+		username = "Unax";
+		amount = 100.00;
+		deposit = false;
+
+		try {
+
+			sut.open();
+
+			double dineroAntes = sut.getActualMoney(username);
+
+			System.out.println("dinero antes del retiro: " + dineroAntes);
+
+			op = sut.gauzatuEragiketa(username, amount, deposit);
+
+			double dineroDespues = sut.getActualMoney(username);
+
+			System.out.println("dinero despues del retiro: " + dineroDespues);
+
+			assertTrue(dineroDespues == 0);
+
+		} catch (Exception e) {
+
+			fail();
+		} finally {
+
+			deposit = true;
+			sut.gauzatuEragiketa(username, amount, deposit);
+			sut.close();
+
+		}
+
+	}
+
+	@Test
+	// the user deposits money in his account successfully
+	public void test5() {
+
+		username = "Urtzi";
+		amount = 25.00;
+		deposit = true;
+
+		try {
+
+			sut.open();
+
+			double dineroAntes = sut.getActualMoney(username);
+
+			System.out.println("dinero antes del ingreso: " + dineroAntes);
+
+			op = sut.gauzatuEragiketa(username, amount, deposit);
+
+			double dineroDespues = sut.getActualMoney(username);
+
+			System.out.println("dinero despues del ingreso: " + dineroDespues);
+
+			assertTrue(dineroDespues == dineroAntes + 25.00);
+
+		} catch (Exception e) {
+
+			fail();
+		} finally {
+
+			deposit = false;
+			sut.gauzatuEragiketa(username, amount, deposit);
+			sut.close();
+
+		}
+
+	}
 }
